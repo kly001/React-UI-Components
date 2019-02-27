d23f3556d059d69add31dc0cc1e32115ffbaf662
Preprocessing - I <karenly1961@gmail.com>
2019-02-27 13:58:22 -0500

    Editing CardContainer

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Card.css';
+
+function CardBanner() {
+    return(
+        <div className="bannerLink">
+        <a className="cardBannerLink" href="https://www.reactjs.org" target="_blank">
+        <img className="bannerImage" src=" https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png" alt="Banner Image"></img>
+        </a>  
+        </div>
+    )
+}
+export default CardBanner;
