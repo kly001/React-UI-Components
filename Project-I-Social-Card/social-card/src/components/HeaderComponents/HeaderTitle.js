60ccf2cc8d1ddaa8f348d523a05d14df36bfd498
Preprocessing - I <karenly1961@gmail.com>
2019-02-27 20:24:17 -0500

    ..

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Header.css';
 
+
 function HeaderTitle() {
+    let d = new Date();
     return (
         <div className="headerTitle">
         <h1>Lambda School</h1>
         <p> @lambda School</p>
-        <p> 27 Feb</p>  
+        <p> 27 Feb </p>
         </div>
     )
 }
